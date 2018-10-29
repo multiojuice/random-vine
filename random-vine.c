d4dac10c813363af33276ac5038bc044f5de1066
Owen J Sullivan <multiojuice@gmail.com>
2018-10-28 23:25:47 -0700

    Getting random vines!

--- random-vine.c
+++ random-vine.c
@@ -7,9 +7,12 @@
 int main()
 {
   FILE *fp;
+  FILE *vines;
   char str[MAXCHAR];
+  char vine_str[MAXCHAR];
   int num_vines;
   char* filename = "./info.txt";
+  char* vines_file = "./vines.txt";
   int random_num;
 
   // Get the amount of vines we have
@@ -27,7 +30,35 @@ int main()
   // Decide which vine number to get
   srand(time(NULL));
   random_num = (rand() % num_vines) + 1;
-  printf("%d", random_num);
+
+  vines = fopen(vines_file, "r");
+  if (fp == NULL){
+      printf("Could not open file %s",vines_file);
+      return 1;
+  }
+
+  char ch;
+  int count = 0;
+
+  while((ch = fgetc(vines)) != EOF)
+          if(ch == '`')
+          {
+            count++;
+            if(count >= random_num){
+              break;  
+            }
+          }
+  int index = 0;
+
+  while ((ch = fgetc(vines)) != '`')
+  {
+    vine_str[index] = ch;
+    index++;
+  }
+
+  printf("%s", vine_str);
+  fclose(vines);
+  
 
   return 0;
 }
